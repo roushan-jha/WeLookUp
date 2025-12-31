aee98d8caf35f5fa3b703bd21d15f54889824a8d
Peteshlok <shlok3pete@gmail.com>
2025-12-31 23:50:07 +0530

    changed backend port numeber from 300 to 8000

--- backend-services/api-gateway/server.js
+++ backend-services/api-gateway/server.js
@@ -16,7 +16,7 @@ import './models/Review.js';
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Enhanced request logger for debugging
